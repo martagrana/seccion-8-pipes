d36ce2d58115694e11465eae04cbb17f364a8690
Marta Grana <marta.grana.fdez@gmail.com>
2024-08-01 17:20:14 +0200

    167. chaining pipes & being aware of limitations

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -6,10 +6,13 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 })
 export class TemperaturePipe implements PipeTransform {
-    transform(value: string | number,
+    transform(value: string | number | null,
         inputType: 'cel' | 'fah',
         outputType?: 'cel' | 'fah'
     ) {
+        if (!value) {
+            return value;
+        }
         let val: number;
         if (typeof value === 'string') {
             val = parseFloat(value);
@@ -32,6 +35,6 @@ export class TemperaturePipe implements PipeTransform {
             symbol = outputType === 'cel' ? 'ºC' : 'ºF';
         }
 
-        return `${outputTemp} ${symbol}`;
+        return `${outputTemp.toFixed(2)} ${symbol}`;
     }
 }
